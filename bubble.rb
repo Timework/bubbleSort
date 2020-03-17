a886ade272419ce7d6801093309fa6fd18de8aea
Timework <timework.bibo@gmail.com>
2020-03-17 18:04:53 -0500

    Finished

--- bubble.rb
+++ bubble.rb
@@ -19,3 +19,29 @@ def bubble(arr)
     print arr
 end
 bubble([4,3,78,2,0,2])
+def bubble_sort_by(arr)
+    sorted = false
+    while sorted == false
+        counter = 0
+        arr.each.with_index do |i, ind|
+            k = ind + 1
+            if k != arr.length
+            checker = yield(i, arr[ind + 1])
+            if checker > 0
+                counter += 1
+                arr[ind], arr[k] = arr[k], arr[ind]
+            end
+        end
+        end
+        if counter == 0
+            sorted = true
+        else
+            counter = 0
+        end
+
+    end
+    print arr
+end
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+     left.length - right.length
+   end
---
+++ readme.md
@@ -0,0 +1,2 @@
+This is the bubble sort for Ruby Programming Project: Advanced Building Blocks
+Project 1 on the Odin Project. Completed : 3/17/2020
